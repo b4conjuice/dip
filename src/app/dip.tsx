4d060153b3874968b3230499af91d297483063db
Daniel Lopez <d@niel.to>
2024-10-30 16:12:17 -0700

    store start locally

--- src/app/dip.tsx
+++ src/app/dip.tsx
@@ -3,11 +3,13 @@
 import { Title } from '@/components/ui'
 import { getBounce } from '@/lib/bounce'
 import useForm from '@/lib/useForm'
+import useLocalStorage from '@/lib/useLocalStorage'
 
 export default function Dip() {
+  const [storedStart, setStoredStart] = useLocalStorage('dip-start', '07:00')
   const { values, handleChange } = useForm({
     initialValues: {
-      start: '07:00',
+      start: storedStart,
       startLunch: '12:00',
       endLunch: '12:30',
     },
@@ -16,11 +18,9 @@ export default function Dip() {
     },
   })
   const { start, startLunch, endLunch } = values
-  const { T: dip } = getBounce(
-    String(start),
-    String(startLunch),
-    String(endLunch)
-  )
+  const { T: dip } = storedStart
+    ? getBounce(String(start), String(startLunch), String(endLunch))
+    : { T: null }
   return (
     <>
       <Title>dip</Title>
@@ -28,23 +28,26 @@ export default function Dip() {
         <input
           name='start'
           value={String(start)}
-          onChange={handleChange}
+          onChange={e => {
+            handleChange(e)
+            setStoredStart(e.currentTarget.value)
+          }}
           type='time'
-          className='bg-cobalt w-full'
+          className='w-full bg-cobalt'
         />
         <input
           name='startLunch'
           value={String(startLunch)}
           onChange={handleChange}
           type='time'
-          className='bg-cobalt w-full'
+          className='w-full bg-cobalt'
         />
         <input
           name='endLunch'
           value={String(endLunch)}
           onChange={handleChange}
           type='time'
-          className='bg-cobalt w-full'
+          className='w-full bg-cobalt'
         />
       </div>
       <p>{dip}</p>
